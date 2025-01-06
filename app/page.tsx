dcd474d4396fff798ca83ccacfd2c0838540c7c5
dev-conquer-codes <rakshit2871@gmail.com>
2025-01-06 11:17:14 +0530

    commit2

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "./_components/HeroSection";
 import ServiceSection from "./_components/ServiceSection";
 import AboutUs from "./_components/AboutUs";
 
-import Footer from "./_components/Footer";
+
 import MessageFromFounder from "./_components/Message";
 import WhatSetsUsApart from "./_components/WhatSetsUsApart";
 import OurPurpose from "./_components/OurPurpose";
